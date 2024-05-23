1ae28fea3b1a7c5ad5c61e82dec093b44e2039a2
Dev Rian <rian.jesus5@etec.sp.gov.br>
2024-05-23 19:52:01 -0300

    second commit

--- src/main/java/com/example/TabelaBrasileirao/TabelaBrasileiraoApplication.java
+++ src/main/java/com/example/TabelaBrasileirao/TabelaBrasileiraoApplication.java
@@ -7,9 +7,22 @@ public class TabelaBrasileiraoApplication {
 
 	public static void main(String[] args) {
          ClubeFutebol santos = new ClubeFutebol("Santos Futebol Club", "Peixei", "Baleião", "Carille") ;
-		 ClubeFutebol sao_paulo = new ClubeFutebol("São Paulo Futebol Club", "Bambi", "Santo Paulo", "Zubeldía");
+		 ClubeFutebol sao_paulo = new ClubeFutebol("São Paulo Futebol Club", "Trikas", "Santo Paulo", "Zubeldía");
 		 ClubeFutebol flamengo = new ClubeFutebol("Clube de Regatas do Flamengo", "Mengão", "Urubu", "Tite");
 		 ClubeFutebol bragantino = new ClubeFutebol("Red Bull Bragantino", "Massa Bruta", "Toro Loko", "Caixinha");
+		 ClubeFutebol cruzeiro = new ClubeFutebol("Cruzeiro Esporte Clube","Raposa", "Raposa", "Fernando Seabra" );
+		 ClubeFutebol portuguesa = new ClubeFutebol("Associação Atlética Portuguesa", "Briosa","Cachopinha", "Sérgio Guedes" );
+		 ClubeFutebol ferroviaria = new ClubeFutebol("Associação Ferroviária de Esportes", "Ferroviaira", "Oswaldinho","Vinícius Bergantin" );
+		 ClubeFutebol paysandu = new ClubeFutebol("Paysandu Sport Club","O Clube de Suiço", "Lobo Mau", "Hélio dos Anjos");
+		 ClubeFutebol quatorze_de_julho = new ClubeFutebol("Esporte Clube 14 de Julho", "Leão da Fronteira", "Leão", "null");
+		 ClubeFutebol fluminense = new ClubeFutebol("Fluminense Football Club", "Flu", "Guerreirinho", "Diniz");
+
+		 // função para sortear um numero de 0 até 7, esse valor será a quantidade de gol do club na partida, função
+		 // deve pegar dois clubs aleatórios, sortear os gols e comparar, com o resultado da comparação, chama determinada função
+
+
+
+
 	}
 
 }
